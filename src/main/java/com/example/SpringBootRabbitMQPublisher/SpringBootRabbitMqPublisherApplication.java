05838c77672846cc179cbf87d813407d88fe035e
albertoliu1993 <albertoliu1993@gmail.com>
2022-05-08 22:11:07 -0700

    first commit

---
+++ src/main/java/com/example/SpringBootRabbitMQPublisher/SpringBootRabbitMqPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRabbitMQPublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitMqPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRabbitMqPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootRabbitMQPublisher/SpringBootRabbitMqPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRabbitMQPublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitMqPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
